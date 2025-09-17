7f489456fe516afc50db44df16fe447d961696bb
HPNChanel <phucnguyen20031976@gmail.com>
2025-09-17 12:25:06 +0700

    Add automatic Github Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quiz-presenter/', // Set the base path for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
